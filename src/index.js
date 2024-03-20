52d365ee7639a1a1e836b59918876f1b97a85053
Rohit Kumar <107547744+thisisrohitkumar@users.noreply.github.com>
2024-03-20 17:15:05 +0530

    create: users route for api endpoint '/api/v1/users'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 require('dotenv').config()
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const ejs = require('ejs')
+const usersRoute = require('./routes/users.route')
 
 const PORT = process.env.PORT || 8000
 const app = express()
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+// Setting templating engine
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./src/views'))
+
+// API Routes
+app.get('/', (req, res) => {
+    return res.render('index')
+})
+
+app.use('/api/v1', usersRoute)
 
 app.listen(PORT, () => {
     console.log(`Server is listening at PORT ${PORT}`)
