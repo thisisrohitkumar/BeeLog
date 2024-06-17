03aa4991f4d9c4d4f5f6999410c9ccb2db9f57a4
Rohit Kumar <107547744+thisisrohitkumar@users.noreply.github.com>
2024-06-17 21:33:40 +0530

    add: styles to profile page | create: edit profile form

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,6 +18,10 @@ const userSchema = new Schema({
         type: String,
         enum: ["user", "admin"],
         default: "user"
+    },
+    dp:{
+        type: String,
+        default: "/images/default_dp.png"
     }
 }, { timestamps : true})
 
--- src/routes/view.route.js
+++ src/routes/view.route.js
@@ -3,6 +3,7 @@ const { checkAuthenticity, checkIfUserLoggedIn } = require("../middlewares/check
 const { checkAuthority } = require("../middlewares/checkAuthority");
 const { verifyToken } = require("../services/auth.service");
 const Blog = require("../models/blog.model");
+const User = require("../models/user.model");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -30,6 +31,16 @@ router.get("/dashboard", checkIfUserLoggedIn, async (req, res) => {
     const user = await verifyToken(token);
     const blogs = await Blog.find({author: user.id}).populate('author').sort([["createdAt", -1]])
     return res.render("dashboard", {user, blogs});
+});
+
+router.get("/profile", checkIfUserLoggedIn, async (req, res) => {
+    const token = req.cookies["jwt"];
+    if (!token) {
+      return res.render("home");
+    }
+    const user = await verifyToken(token);
+    const userDetails = await User.findOne({_id: user.id});
+    return res.render("profile", {user, userDetails});
 });
 
 router.get("/addBlog", checkIfUserLoggedIn, async (req, res) => {
